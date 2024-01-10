6eeabdd8012340c8e453322a72ac6bca04beaedc
AISHWARYA139 <aishmore2304@gmail.com>
2024-01-10 14:43:59 +0530

    commit all weather forcasting files

---
+++ weather/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "739e102dd925bd7b83aac633610bb0e6",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
